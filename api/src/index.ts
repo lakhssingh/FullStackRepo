0f3ef329ca76c79ebaa964a4dd0ab53c54f6eadf
Lakhs <lakhbir.shah@yahoo.co.uk>
2024-10-16 20:59:18 +0100

    deploy to genezio

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products/index.js";
 import authRoutes from "./routes/auth/index.js";
+import severless from "serverless-http";
 
 const port = 3000;
 
@@ -16,6 +17,10 @@ app.get("/", (req, res) => {
 app.use("/products", productsRoutes);
 app.use("/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`Example 123n ${port}`);
-});
+if (process.env.NODE_ENV === "dev") {
+  app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+  });
+}
+
+export const handler = severless(app);
